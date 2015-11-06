27cc5619cbb7876060ff462975863e5e61e1b8d7
Moisés Pio <live.moises@gmail.com>
2015-11-06 00:14:51 -0200

    Feed

---
+++ ajudae/Controller/FeedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FeedViewController.swift
+//  ajudae
+//
+//  Created by Moisés Pio on 11/6/15.
+//  Copyright © 2015 Moisés Pio. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
