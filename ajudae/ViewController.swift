07db722797fb32496de6fa53f84d06a36eb990db
Moisés Pio <live.moises@gmail.com>
2015-11-05 23:26:38 -0200

    Initial Commit

---
+++ ajudae/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ajudae
+//
+//  Created by Moisés Pio on 11/5/15.
+//  Copyright © 2015 Moisés Pio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ajudaeTests/ajudaeTests.swift
@@ -0,0 +1,36 @@
+//
+//  ajudaeTests.swift
+//  ajudaeTests
+//
+//  Created by Moisés Pio on 11/5/15.
+//  Copyright © 2015 Moisés Pio. All rights reserved.
+//
+
+import XCTest
+@testable import ajudae
+
+class ajudaeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
