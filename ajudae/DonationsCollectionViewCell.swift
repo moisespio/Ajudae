8f172fdc20d1b4389f6f8cf0e2e8098a418d1a5e
Moisés Pio <live.moises@gmail.com>
2015-11-06 02:15:41 -0200

    donations

---
+++ ajudae/DonationsCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DonationsCollectionViewCell.swift
+//  ajudae
+//
+//  Created by Moisés Pio on 11/6/15.
+//  Copyright © 2015 Moisés Pio. All rights reserved.
+//
+
+import UIKit
+
+class DonationsCollectionViewCell: UICollectionViewCell {
+    
+    override func drawRect(rect: CGRect) {
+        self.layer.cornerRadius = 4
+        self.clipsToBounds = true
+    }
+    
+}
